f58d2b4ad428163a8657ce6f9e4eee2caf6e933c
Mikhail Remizov <00keng@gmail.com>
2015-03-03 15:24:56 +0300

    first somewhat working version
    changed byte reading function, added reading coords, implemented the
    main program cycle

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 all: keng_esp_hack
 
 keng_esp_hack: esp.o
-	gcc esp.o -o keng_esp_hack
+	gcc esp.o -o keng_esp_hack -lpsapi
 
-esp.o:
+esp.o: esp.c
 	gcc -c esp.c
 
 clean:
--- esp.c
+++ esp.c
@@ -1,62 +1,95 @@
 #include <windows.h>
 #include <stdio.h>
+#include <psapi.h>
+#include <string.h>
+#include <time.h>
 
 #define WINDOWNAME "Counter-Strike: Global Offensive"
-#define PLAYERLIST 0x2D62C710
-#define HPOFFSET 0x214
-#define PLAYERSZ 0x18
-/*#define PLAYERSNUMADDR 0x5DE6CCB8*/
+#define PLRSZ 0x18
+#define SERVERDLL "server.dll"
 
 const DWORD plr_num_addr = 0x5DE6CCB8;
+const DWORD plr_num_offset = 0x93CCB8;
+const DWORD plr_list_offset = 0x0089944C;
+const DWORD hp_offset = 0x214;
+const DWORD coords_offset = 0x1D0;
+DWORD server_dll_base;
 HANDLE get_process_handle();
-BOOL read_four_bytes(PCVOID addr, void* buf);
+BOOL read_bytes(PCVOID addr, int num, void* buf);
 HANDLE hProcess;
 
 int main(int argc, char** argv)
 {
-    printf("main fired\n");
 	HANDLE h = get_process_handle();
     hProcess = h;
-	int buf = 0;
-	BOOL r = read_four_bytes((PCVOID)plr_num_addr, &buf);
+	DWORD addr = server_dll_base + plr_list_offset;
+	printf("player list offset: %08X\n", addr);
+	int players_on_map, i, hp;
+	float coords[3];
+	DWORD plr_addr;
+	read_bytes((PCVOID)addr, 4, &plr_addr);
+	printf("players list addr: %08X\n", plr_addr);
+	for (;;Sleep(500)) {
+		system("cls");
+		printf("tick\n");
+		read_bytes((PCVOID)(server_dll_base + plr_num_offset), 4, 
+				&players_on_map);
+		printf("players on the map: %d\n", players_on_map);
+		for (i = 0; i < players_on_map; i++) {
+			read_bytes((PCVOID)(addr+i*PLRSZ), 4, &plr_addr);
+			read_bytes((PCVOID)(plr_addr + hp_offset), 4, &hp);
+			read_bytes((PCVOID)(plr_addr + coords_offset), 12, &coords);
+			printf("player %d health: %d (%g:%g:%g)\n", i, hp,
+					coords[0], coords[1], coords[2]);
+		}
+	}
 	CloseHandle(h);
-	printf("read value = %d\n", buf);
-	printf("main finished\n");
     return 0;
 }
 
 HANDLE get_process_handle()
 {
-	printf("get_process_handle fired\n");
 	HANDLE h = 0;
+	DWORD pid = 0;
 	HWND hWnd = FindWindow(NULL, WINDOWNAME);
 	if (hWnd == NULL) {
 		printf("FindWindow failed, %08X\n", GetLastError());
 		return h;
 	}
 	printf("hWnd = %08X\n", hWnd);
-	DWORD pid;
 	GetWindowThreadProcessId(hWnd, &pid);
-	h = OpenProcess(PROCESS_VM_READ, 0, pid);
+	h = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, 0, pid);
 	if (h == NULL) {
 		printf("OpenProcess failed, %08X\n", GetLastError());
 		return h;
 	}
 	printf("process handle = %08X\n", h);
-	printf("get_process_handle finished\n");
+	HMODULE hMods[1024];
+	int i;
+	if (EnumProcessModules(h, hMods, sizeof(hMods), &pid) == FALSE) {
+		printf("enumprocessmodules failed, %08X\n", GetLastError());
+	} else {
+	    for (i = 0; i < (pid / sizeof(HMODULE)); i++) {
+		    TCHAR szModName[MAX_PATH];
+		    if (GetModuleFileNameEx(h, hMods[i], szModName,
+					sizeof(szModName)/sizeof(TCHAR))) { 
+				if (strstr(szModName, SERVERDLL) != NULL) {
+					printf("server.dll base: %08X\n", hMods[i]);
+					server_dll_base = (DWORD)hMods[i];
+				}
+			}
+		}
+	}	
 	return h;
 }
 
-BOOL read_four_bytes(PCVOID addr, void* buf)
+BOOL read_bytes(PCVOID addr, int num, void* buf)
 {
-	printf("read_four_bytes fired\n");
-	printf("addr = %08X\n", addr);
 	SIZE_T sz = 0;
-    BOOL r = ReadProcessMemory(hProcess, addr, buf, 4, &sz);
+    BOOL r = ReadProcessMemory(hProcess, addr, buf, num, &sz);
 	if (r == FALSE || sz == 0) {
         printf("RPM error, %08X\n", GetLastError());
 		return FALSE;
 	}
 	return TRUE;
-	printf("read_four bytes finished\n");
 }
